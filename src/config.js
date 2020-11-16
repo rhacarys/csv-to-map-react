b32f627babe6be8cfe6b1c7d8cb4bb5be085671e
Nathaniel Lacerda <nathaniel.lacerda@gmail.com>
2020-11-16 10:07:50 -0300

    Environment config

--- src/config.js
+++ src/config.js
@@ -1,5 +1,10 @@
+const BASE_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://csv-to-map-api.herokuapp.com/"
+    : "http://localhost:8000";
+
 const config = {
-  API_URL: "http://localhost:8000/api/v1",
-  API_FILES_ENDPOINT: 'http://localhost:8000/api/v1/files',
-}
-export default config
+  API_URL: `${BASE_URL}/api/v1`,
+  API_FILES_ENDPOINT: `${BASE_URL}/api/v1/files`,
+};
+export default config;
